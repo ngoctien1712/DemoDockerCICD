83eff2896571a9031bc49d1277ca2a9617e98cfe
NgocTien1712 <huynhngoctien1712@gmail.com>
2026-03-20 15:48:35 +0700

    Khởi tạo dự án

---
+++ src/main/java/com/example/ngoctien/NgoctienApplication.java
@@ -0,0 +1,13 @@
+package com.example.ngoctien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoctienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgoctienApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ngoctien/NgoctienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ngoctien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoctienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
